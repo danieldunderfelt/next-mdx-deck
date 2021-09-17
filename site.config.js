41604aa156129f5bce5c2efb4981ff24a6a7439b
Daniel Dunderfelt <daniel@developsuperpowers.com>
2021-09-17 15:17:24 +0300

    Update node and deps.

--- site.config.js
+++ site.config.js
@@ -1,9 +1,9 @@
 export const siteConfig = {
-  name: `@whoisryosuke`,
+  name: `@ddunderfelt`,
   title: `NextJS MDX Deck`,
-  date: `April 22, 2020`,
+  date: `September 17, 2021`,
   author: {
-    url: `https://twitter.com/whoisryosuke`,
+    url: `https://twitter.com/ddunderfelt`,
   },
 }
 
